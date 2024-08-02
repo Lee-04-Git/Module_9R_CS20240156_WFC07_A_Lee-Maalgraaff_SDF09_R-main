026439bc3f82d91fbfd5e42a6466ea0973d452e3
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-02 13:25:21 +0200

    Add newCard() logic to check game status before drawing

--- index.js
+++ index.js
@@ -52,9 +52,11 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = getRandomCard();
-    sum += card;
-    cards.push(card);
-    console.log(cards);
-    renderGame();
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
